aaa13a89e89b0626cbd783db49737da034586726
uzukozzi <uzukozzi@gmail.com>
2026-07-22 16:38:01 +0200

    Add exercises and palindrome app

---
+++ palindrome/palindrome.js
@@ -0,0 +1,30 @@
+let word = String(prompt("Please enter the word: "));
+let numberOfLetters = word.length;
+
+function isPalindrome(word) {
+    let lettersArray = word.split("");
+
+    for (let i = 0; i < numberOfLetters / 2; i++) {
+        if (lettersArray[i] !== lettersArray[numberOfLetters - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let result = isPalindrome(word);
+
+if (result === false) {
+
+    console.log(`The word "${word}" is not palindrome`);
+
+} else {
+    if (result === true && (numberOfLetters === 0 || numberOfLetters === 1)) {
+
+        console.log(`The word "${word}" is palindrome but the trick work better if you type a word with more than one letter`);
+
+    } else {
+
+        console.log(`The word "${word}" is palindrome`);
+    }
+}
